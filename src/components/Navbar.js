0e3c7108bfbec42c7fc2e05d3b70da673da8e243
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-03-24 10:10:32 -0300

    Adição da barra de pesquisa na navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { FiSearch } from "react-icons/fi"
 import { VscChevronDown, VscChevronUp } from 'react-icons/vsc';
 
-import { Avatar, Bar, DropdownLogout, Logo, Logout, RightDivContent } from "../styles/Navbar";
+import { Avatar, Bar, DropdownLogout, Logo, Logout, RightDivContent, SearchContentDiv, SearchDiv, SearchInput, SearchResultDiv, SearchResultImage, SearchResultsDiv, SearchResultText } from "../styles/Navbar";
 
 export function NavBar() {
     const [open, setOpen] = useState(false);
+    const [canShowResultBar, setCanShowResultBar] = useState(false);
+    const [searchResult, setSearchResult] = useState([]);
     const [avatar, setAvatar] = useState('https://filestore.community.support.microsoft.com/api/images/6061bd47-2818-4f2b-b04a-5a9ddb6f6467?upload=true');
 
     async function getUserInfos() {
@@ -26,23 +29,60 @@ export function NavBar() {
         }
     }
 
+    async function searchUserByUsername(event) {
+        if (event.target.value) {
+            const token = localStorage.getItem('token');
+            try {
+                const result = await axios.post(`${process.env.REACT_APP_API_URL}/search`, { username: event.target.value }, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    }
+                });
+                setSearchResult(result.data);
+            } catch (error) {
+                console.log(error.response);
+                alert(error.response.data);
+            }
+        } else {
+            setSearchResult([]);
+        }
+    }
+
+    function hideSearchBar(event) {
+        if (event.relatedTarget instanceof HTMLAnchorElement === false) setCanShowResultBar(false);
+    }
+
     useEffect(() => {
         getUserInfos();
     }, []);
 
     return (
-        <Bar>
-            <Logo to="/timeline">linkr</Logo>
-            <RightDivContent onClick={() => setOpen(!open)}>
-                {open ? 
-                    <VscChevronUp color="#ffffff" size={30} /> :
-                    <VscChevronDown color="#ffffff" size={30} />
-                }
-                <Avatar data-test="avatar" src={avatar} alt="avatar" />
-                {open && <DropdownLogout>
-                        <Logout to="/logout">Logout</Logout>
-                    </DropdownLogout>}
-            </RightDivContent>
-        </Bar>
+        <>
+            <Bar>
+                <Logo to="/timeline">linkr</Logo>
+                <SearchContentDiv>
+                    <SearchDiv>
+                        <SearchInput placeholder="Search for people" onBlur={hideSearchBar} onChange={searchUserByUsername} onFocus={() => setCanShowResultBar(true)} />
+                        <FiSearch color="#C6C6C6" size={20}/>
+                    </SearchDiv>
+                    <SearchResultsDiv canShowResultBar={canShowResultBar}>
+                        {searchResult.length === 0 ? <SearchResultText>No user found.</SearchResultText> : searchResult.map((result, index) => <SearchResultDiv key={index} to={`/user/${result.id}`}>
+                            <SearchResultImage src={result.image} />
+                            <SearchResultText>{result.username}</SearchResultText>
+                        </SearchResultDiv>)}
+                    </SearchResultsDiv>
+                </SearchContentDiv>
+                <RightDivContent onClick={() => setOpen(!open)}>
+                    {open ? 
+                        <VscChevronUp color="#FFFFFF" size={30} /> :
+                        <VscChevronDown color="#FFFFFF" size={30} />
+                    }
+                    <Avatar data-test="avatar" src={avatar} alt="avatar" />
+                    {open && <DropdownLogout>
+                            <Logout to="/logout">Logout</Logout>
+                        </DropdownLogout>}
+                </RightDivContent>
+            </Bar>
+        </>
     );
 };
--- src/styles/Navbar.js
+++ src/styles/Navbar.js
@@ -22,6 +22,93 @@ export const Logo = styled(Link)`
     cursor: pointer;
 `;
 
+export const SearchContentDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+`;
+
+export const SearchDiv = styled.div`
+    background-color: #FFFFFF;
+    display: flex;
+    align-items: center;
+    height: 42px;
+    margin-top: 5px;
+    border-radius: 8px;
+    gap: 10px;
+    z-index: 2;
+`;
+
+export const SearchInput = styled.input`
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    color: #333333;
+    border: none;
+    margin-left: 15px;
+    width: 88%;
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+export const AfterBarDiv = styled.div`
+    display: ${props => props.canShowResultBar ? "flex" : "none"};
+    position: absolute;
+    width: 100%;
+    justify-content: center;
+    padding: 10px;
+    box-sizing: border-box;
+`;
+
+export const SearchResultsDiv = styled.div`
+    position: absolute;
+    box-sizing: border-box;
+    padding: 10px;
+    width: 39.4%;
+    margin-top: 41px;
+    z-index: 1;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    border-end-start-radius: 8px;
+    border-end-end-radius: 8px;
+    background-color: #E7E7E7;
+    display: ${props => props.canShowResultBar ? "flex" : "none"};
+    ${props => props.canShowResultBar ? "flex-direction: column;" : ""}
+    ${props => props.canShowResultBar ? "gap: 20px;" : ""}
+`;
+
+export const SearchResultsContent = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const SearchResultDiv = styled(Link)`
+    display: flex;
+    margin-left: 10px;
+    text-decoration: none;
+    align-items: center;
+    gap: 4px;
+`;
+
+export const SearchResultImage = styled.img`
+    width: 39px;
+    height: 39px;
+    object-fit: fill;
+    border-radius: 50%;
+`;
+
+export const SearchResultText = styled.p`
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 19px;
+    margin-left: 10px;
+    color: #515151;
+`;
+
 export const RightDivContent = styled.div`
     margin-right: 10px;
     display: flex;
